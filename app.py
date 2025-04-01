6f42f4aea199db8e594d0f46bae7e39f0221c0ff
sanjeevani6 <150465527+sanjeevani6@users.noreply.github.com>
2025-04-02 02:09:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -219,4 +219,4 @@ def download_pdf(filename):
     return send_file(generated_results[filename]["pdf_filename"], as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',port=10000,debug=True)
